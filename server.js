a265c7c51538805f5ebac22c8a84c5567ef3f03b
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-03-27 16:51:03 -0300

    Fix caminho server.js

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 app.use(express.json());
 app.use('/api/transacoes', transacoesRouter);
 
+const buildPath = path.resolve(__dirname, '../client/build');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(buildPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
