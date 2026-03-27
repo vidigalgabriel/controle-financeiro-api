f2491683940f22982d0d375e1b793a657ef0c4dd
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-03-27 17:45:28 -0300

    fix: corrige rotas da api

--- src/routes/transacaoRoutes.js
+++ src/routes/transacaoRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const transacaoController = require('../controllers/transacaoController');
 
-router.post('/transacoes', (req, res) => transacaoController.criar(req, res));
-router.get('/transacoes', (req, res) => transacaoController.listarTodas(req, res));
-router.get('/transacoes/:id', (req, res) => transacaoController.buscarPorId(req, res));
-router.put('/transacoes/:id', (req, res) => transacaoController.atualizar(req, res));
-router.delete('/transacoes/:id', (req, res) => transacaoController.deletar(req, res));
+router.post('/', (req, res) => transacaoController.criar(req, res));
+router.get('/', (req, res) => transacaoController.listarTodas(req, res));
+router.get('/:id', (req, res) => transacaoController.buscarPorId(req, res));
+router.put('/:id', (req, res) => transacaoController.atualizar(req, res));
+router.delete('/:id', (req, res) => transacaoController.deletar(req, res));
 
 router.get('/relatorios/resumo', (req, res) => transacaoController.obterResumo(req, res));
 router.get('/relatorios/categoria', (req, res) => transacaoController.obterTotalPorCategoria(req, res));
